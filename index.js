1e531b8943ae2cd03c924ca7476ed886c4981f42
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-28 13:54:51 +0100

    :sparkles: Scaffold simple web server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require('express')
+// const cors = require('cors')
+// bring in the model so we can talk to the db
+const db = require('./data/db')
+
+const app = express()
+
+// app.use(cors())
+app.use(express.json())
+
+app.get('*', handleDefaultRequest)
+
+
+
+function handleDefaultRequest(req, res) {
+    res.json('Read docs properly, dude')
+  }
+  
+  app.listen(process.env.PORT || 3001, () => {
+    console.log('listening on ' + (process.env.PORT || 3001));
+  })
