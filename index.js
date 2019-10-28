8bd1333d5720a9ac38a6257d9d455cebe5aa0b75
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-28 14:13:32 +0100

    :sparkles: Create get single user endpoint

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const app = express()
 app.use(express.json())
 
 app.get('/api/users/', getAllUsers)
+app.get('/api/users/:id', getSingleUser)
 app.get('*', handleDefaultRequest)
+
+function getSingleUser (req, res) {
+    db.findById(req.params.id)
+    .then(data => res.status(200).json(data))
+    .catch(err => console.log(err))
+}
 
 function getAllUsers (req, res) {
     db.find()
